ad171f9c3696866d04baf173e1d409e04c890cb3
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-04-26 14:21:24 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,22 @@
+- Produkte hinzufügen und bearbeiten
+- Bestellungen einsehen (bearbeitet/ nicht bearbeitet)
+- Gesamtanzahl Produkte einsehen
+- Newsletter
+
+product{
+title,
+category,
+description,
+variations,
+price,
+stock,
+image
+}
+
+order{
+date,
+products,
+state,
+price,
+customer
+}
---
+++ backend/utils/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+const MONGO_URL = process.env.MONGO_URL;
+const MONGO_DB = process.env.MONGO_DB;
+
+const client = new MongoClient(MONGO_URL);
+
+let db;
+
+export const getDB = async () => {
+	if (db) return db;
+	else {
+		await client.connect();
+		db = client.db(DB);
+		return db;
+	}
+};
